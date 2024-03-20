b33a1e7e4e6c9dda19d1c73fc3c1f8a034b68c6f
jihyunlee2023 <152181713+jihyunlee2023@users.noreply.github.com>
2024-03-21 01:27:09 +0900

    Add files via upload

--- homework2-2.c
+++ homework2-2.c
@@ -3,53 +3,55 @@
 int main()
 {
     printf("[----- [이지현]  [2023078007] -----]");
+
     int i;
     int *ptr;
     int **dptr;
 
-    i = 1234;
+    i = 1234; // i에 1234 값을 할당합니다.
     
     printf("[checking values before ptr = &i] \n"); 
-    printf("value of i == %d\n", i); 
-    printf("address of i == %p\n", &i); 
-    printf("value of ptr == %p\n", ptr); 
-    printf("address of ptr == %p\n", &ptr);
+    printf("value of i == %d\n", i); // i 변수의 값 출력
+    printf("address of i == %p\n", &i); // i 변수의 주소 출력
+    printf("value of ptr == %p\n", ptr); // ptr 변수가 가리키는 주소 출력 
+    printf("address of ptr == %p\n", &ptr); // ptr 변수의 주소 출력
 
-    ptr = &i; /* ptr is now holding the address of i */
+    ptr = &i;  // ptr 변수에 i의 주소를 할당합니다.
 
     printf("\n[checking values after ptr = &i] \n"); 
-    printf("value of i == %d\n", i); 
-    printf("address of i == %p\n", &i); 
-    printf("value of ptr == %p\n", ptr); 
-    printf("address of ptr == %p\n", &ptr); 
-    printf("value of *ptr == %d\n", *ptr);
+    printf("value of i == %d\n", i); // i 변수의 값 출력
+    printf("address of i == %p\n", &i); // i 변수의 주소 출력
+    printf("value of ptr == %p\n", ptr); // ptr 변수가 가리키는 주소 출력
+    printf("address of ptr == %p\n", &ptr); // ptr 변수의 주소 출력
+    printf("value of *ptr == %d\n", *ptr); // ptr 변수가 가리키는 주소에 저장된 값 출력 
 
-    dptr = &ptr;
+    dptr = &ptr; // dptr 변수에 ptr 변수의 주소를 할당합니다.
 
     printf("\n[checking values after dptr = &ptr] \n"); 
-    printf("value of i == %d\n", i);
-    printf("address of i == %p\n", &i);
-    printf("value of ptr == %p\n", ptr); 
-    printf("address of ptr == %p\n", &ptr); 
-    printf("value of *ptr == %d\n", *ptr); 
-    printf("value of dptr == %p\n", dptr); 
-    printf("address of dptr == %p\n", &dptr);
-    printf("value of *dptr == %p\n", *dptr);
-    printf("value of **dptr == %d\n", **dptr);
+    printf("value of i == %d\n", i); // i 변수의 값 출력
+    printf("address of i == %p\n", &i); // i 변수의 주소 출력
+    printf("value of ptr == %p\n", ptr); // ptr 변수가 가리키는 주소 출력 
+    printf("address of ptr == %p\n", &ptr); // ptr 변수의 주소 출력
+    printf("value of *ptr == %d\n", *ptr); // ptr 변수가 가리키는 주소에 저장된 값 출력 
+    printf("value of dptr == %p\n", dptr); // dptr 변수가 가리키는 주소 출력
+    printf("address of dptr == %p\n", &dptr); // dptr 변수의 주소 출력
+    printf("value of *dptr == %p\n", *dptr); // dptr 변수가 가리키는 주소에 저장된 값 출력 
+    printf("value of **dptr == %d\n", **dptr); // dptr 변수가 가리키는 주소에 저장된 값의 주소에 저장된 값 출력
 
-    *ptr = 7777;
+    *ptr = 7777; // ptr이 가리키는 곳에 7777을 저장합니다.
 
     printf("\n[after *ptr = 7777] \n");
-    printf("value of i == %d\n", i); 
-    printf("value of *ptr == %d\n", *ptr);
-    printf("value of **dptr == %d\n", **dptr);
+    printf("value of i == %d\n", i); // i 변수의 값 출력 
+    printf("value of *ptr == %d\n", *ptr); // ptr 변수가 가리키는 곳에 저장된 값 출력
+    printf("value of **dptr == %d\n", **dptr); // dptr 변수가 가리키는 주소에 저장된 값의 주소에 저장된 값 출력
 
-    **dptr = 8888;
+    **dptr = 8888; // dptr이 가리키는 곳에 8888을 저장합니다.
 
     printf("\n[after **dptr = 8888] \n");
-    printf("value of i == %d\n", i); 
-    printf("value of *ptr == %d\n", *ptr); 
-    printf("value of **dptr == %d\n", **dptr);
+    printf("value of i == %d\n", i); // i 변수의 값 출력 
+    printf("value of *ptr == %d\n", *ptr); // ptr 변수가 가리키는 곳에 저장된 값 출력 
+    printf("value of **dptr == %d\n", **dptr); // dptr 변수가 가리키는 주소에 저장된 값의 주소에 저장된 값 출력
 
     return 0;
 }
+
